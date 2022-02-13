24020e911151a22e8153949a3a175c443e62d30f
20163093 <kimsung97@naver.com>
2022-02-13 18:25:28 +0900

    trying to change library from bs4 to selenium

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import requests
 
+driver = webdriver.Chrome('./chromedriver')
 url = requests.get('https://comic.naver.com/webtoon/weekday')
 soup = BeautifulSoup(url.text, 'html.parser')
 daily_webtoon = soup.find('div', {'class':'end_page'}).find('div', id='container').find('div', id='content').find('div', {'class':'list_area daily_all'})
@@ -18,12 +20,23 @@ def extract_webtoon_info():
             info.append(each_info)
     return info
 
-def find_series_link_and_move(webtoon_info):
-    for webtoon in webtoon_info:
-        # print(webtoon['link'])
-        webtoon_url = requests.get('https://comic.naver.com/' + webtoon['link'])
-        link_soup = BeautifulSoup(webtoon_url.text, 'html.parser')
-        print(webtoon_url.status_code)
+def find_series_url_and_move(webtoon_info):
+    # for webtoon in webtoon_info:
+    webtoon_url = requests.get('https://comic.naver.com/' + webtoon_info['link'])
+    print('https://comic.naver.com/' + webtoon_info['link'])
+    print(webtoon_url)
+    link_soup = BeautifulSoup(webtoon_url.text, 'html.parser')
+    try:
+        find_url = link_soup.find('div', {'class':'end_page'}).find('div', id='container').find('div', id='content')
+        # .find('table', {'class':'viewList'})
+        # .find('tbody', {'class':'band_banner_v2'}).find('td').find('a', href=True)['href']
+        # webtoon['series_url'] = find_url
+        # print(find_url.prettify())
+    except:
+        pass
+        # webtoon['series_url'] = None
+    # print(webtoon_info)
 
 webtoon_info = extract_webtoon_info()
-find_series_link_and_move(webtoon_info)
+print(webtoon_info[0])
+find_series_url_and_move(webtoon_info[0])
